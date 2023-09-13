c82b99833b16b20754f9aaac609be6927f9e1ecd
Amanmishra03 <aman274501mishra@gmail.com>
2023-09-13 23:03:14 +0530

    digipodium

---
+++ loops/side_io.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+tim = Turtle()
+my_screen = Screen()
+num_sides = my_screen.numinput(title="no. of sides you want", prompt="how many sides you want")
+num_sides = int(num_sides)
+angle = 360/num_sides
+for i in range(num_sides):
+    tim.fd(100)
+    tim.lt(angle)
+
+
+
+
+
+my_screen.exitonclick()
