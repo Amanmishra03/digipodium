efdc1815268e7aedd3266152a6ed0a982c9721dd
Amanmishra03 <aman274501mishra@gmail.com>
2023-10-31 00:26:36 +0530

    streamlit basic

--- immigration_analysis_app/home.py
+++ immigration_analysis_app/home.py
@@ -7,7 +7,7 @@ import numpy as np
 st.set_page_config(
     layout="wide",
     page_title="Immigration Analysis App",
-    page_icon="⚪"
+    page_icon="⚪🧊"
 )
 
 years = list(range(1980,2014))
@@ -27,20 +27,52 @@ def load_data(path):
 with st.spinner('processing imagination data ....'):
     df = load_data('canada.xlsx')
     
-# st.image(" ",
-        #  use_column_width=True)
+st.image("https://blog.mbeforyou.com/wp-content/uploads/2020/01/CANADIAN-IMMIGRATION.jpg",
+          use_column_width=True)
 
-st.title("immigration Analysis App")  
+st.title("Immigration Analysis App")  
 
 c1, c2, c3, c4, c5, c6 = st.columns(6)
 
 total_countries = df.shape[0]
 duration = "1980 - 2013"
 total_immigration = df['Total'].sum()
+st.header("Data summary")
 st.metric("Total Countries", total_countries)
 st.metric("years", duration)
 st.metric("Total Immigration", total_immigration)
 c1.metric("Total Countries", total_countries)
 c2.metric("years", duration)
 c3.metric("Total Immigration", total_immigration)
-st.header("Immgiration analysis")
+
+st.header("Immigration visualization")
+fig = px.line(df, x=df.index, y='Total', height=500)
+st.plotly_chart(fig, use_container_width=True)
+
+top_countries = df.sort_values(by='Total', ascending=False).head(25)
+
+c1, c2 = st.columns([1,3])
+
+limit = c2.slider("Select number of countries", 1, 25, value=5)
+countries = top_countries.index.tolist()[:limit]
+countries_df = df.loc[countries, years].T
+fig2 = px.area(countries_df, x=countries_df.index, y=countries_df.columns)
+
+c1.dataframe(top_countries)
+c2.plotly_chart(fig2, use_container_width=True)
+
+st.subheader("Trend Comparison")
+c1, c2 = st.columns([1,3])
+country_list = df.index.to_list()
+countries = c2.multiselect("Select Countries", country_list)
+if countries:
+    countries_df = df.loc[countries, years].T
+    fig3 = px.line(
+        countries_df,
+        x=countries_df.index,
+        y=countries_df.columns
+    )
+    for country in countries:
+        c1.info(f'{country}: {df.loc[country, "Total"]} Immigrations')
+    c2.plotly_chart(fig3, use_container_width=True)
+    st.toast('Your graph has been loaded!', icon='😍')
